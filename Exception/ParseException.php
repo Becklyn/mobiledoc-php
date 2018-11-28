468091c148a49651669788930e40ddf3d456f0c1
Jannik Zschiesche <jz@becklyn.com>
2018-11-28 21:22:42 +0100

    Add more context to parse exceptions

--- Exception/ParseException.php
+++ Exception/ParseException.php
@@ -3,7 +3,30 @@
 namespace Becklyn\Mobiledoc\Exception;
 
 
+use Becklyn\Mobiledoc\Parser\Html\Node\ElementNode;
+use Becklyn\Mobiledoc\Parser\Html\Node\HtmlNode;
+
+
 class ParseException extends MobiledocException
 {
+    /**
+     *
+     * @param string                    $message
+     * @param null|HtmlNode|\DOMNode    $node
+     * @param \Throwable|null $throwable
+     */
+    public function __construct (string $message, $node = null, ?\Throwable $throwable = null)
+    {
+        if ($node instanceof ElementNode)
+        {
+            $node = $node->getElement();
+        }
 
+        if ($node instanceof \DOMNode)
+        {
+            $message .= " In HTML: '{$node->ownerDocument->saveHTML($node)}'";
+        }
+
+        parent::__construct($message, $throwable);
+    }
 }
