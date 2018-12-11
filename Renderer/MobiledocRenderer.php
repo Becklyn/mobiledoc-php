40daa04940fe11339585f37d8c0761a8c2c90bd6
Jannik Zschiesche <jz@becklyn.com>
2018-12-11 19:16:36 +0100

    Add proper attribute rendering + markup attributes visitors
    * Transform the attributes to a proper map
    * Add multiple types of values that are supported to be rendered to attributes
    * add visitor list that can modify the list of attributes

--- Renderer/MobiledocRenderer.php
+++ Renderer/MobiledocRenderer.php
@@ -4,6 +4,7 @@ namespace Becklyn\Mobiledoc\Renderer;
 
 use Becklyn\Mobiledoc\Mobiledoc\SimpleDocument;
 use Becklyn\Mobiledoc\Extension\ExtensionRegistry;
+use Becklyn\Mobiledoc\Renderer\Markup\MarkupAttributesVisitor;
 
 
 class MobiledocRenderer
@@ -15,11 +16,32 @@ class MobiledocRenderer
 
 
     /**
-     * @param ExtensionRegistry $extensionRegistry
+     * @var MarkupAttributesVisitor[]
      */
-    public function __construct (ExtensionRegistry $extensionRegistry)
+    private $markupAttributesVisitor = [];
+
+
+    /**
+     * @param ExtensionRegistry         $extensionRegistry
+     * @param MarkupAttributesVisitor[] $markupAttributesVisitors
+     */
+    public function __construct (ExtensionRegistry $extensionRegistry, array $markupAttributesVisitors = [])
     {
         $this->extensionRegistry = $extensionRegistry;
+
+        foreach ($markupAttributesVisitors as $visitor)
+        {
+            $this->registerMarkupAttributesVisitor($visitor);
+        }
+    }
+
+
+    /**
+     * @param MarkupAttributesVisitor $visitor
+     */
+    public function registerMarkupAttributesVisitor (MarkupAttributesVisitor $visitor)
+    {
+        $this->markupAttributesVisitor[] = $visitor;
     }
 
 
@@ -36,7 +58,7 @@ class MobiledocRenderer
             return null;
         }
 
-        $render = new RenderProcess($mobiledoc, $this->extensionRegistry);
+        $render = new RenderProcess($mobiledoc, $this->extensionRegistry, $this->markupAttributesVisitor);
         return $render->getRenderedDocument();
     }
 }
